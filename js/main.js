4da595f0686388c871bec6727505b865bf341431
niceusts <niceusantos1365@gmail.com>
2021-05-22 11:29:05 -0300

    Testando funções do JS

--- js/main.js
+++ js/main.js
@@ -1,7 +1,47 @@
+var d = new Date();
+alert(d);
+
+/*
+for(var count = 0; count <=5; count++){
+    alert(count);
+}
+*/
+/*var count = 1;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+/*var idade = prompt("qual sua idade?");
+if (idade >= 18){
+    alert("maior de idade");
+}else{
+    alert("menor de idade");
+}
+*/
+/*var frutas = [{nome:"maça", cor:"vermelha"}, {nome:"uva", cor:"roxa"}];
+console.log(frutas);
+alert(frutas[1].cor);
+
+var fruta = {nome:"maça", cor:"vermelha"};
+console.log(fruta.nome);
+
+var lista = ["pera", "uva", "maça"];
+lista.push("laranja");
+lista.pop();
+console.log(lista);
+console.log(lista.length);
+console.log(lista.reverse());
+console.log(lista.toString());
+console.log(lista.join(" - "));
+
 var nome = "Niceu Biriba";
 var idade = 20;
 var idade2 = 18;
 alert(idade + idade2);
-
+var frase = "Eu nao tenho carro";
+console.log(frase.replace("carro", "moto"));
 console.log(idade);
 console.log(idade2);
+console.log(frase.toUpperCase());
+console.log(frase.toLowerCase());*/
